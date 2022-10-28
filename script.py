1c062d486092209443b466aeb19b8d2f8f13ecb5
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-28 15:13:24 +0200

    update

--- script.py
+++ script.py
@@ -12,6 +12,9 @@ def resize_image(image, new_width=100):
     return(resized_image)
 
 # convert each pixel to grayscale
+def grafify(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
 
 # convert pixels to a string of ASCII characters
 

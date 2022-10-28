1ad3df8650c6af101683f1c3c136d5f86946d83e
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-28 15:27:57 +0200

    update

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ def resize_image(image, new_width=100):
     return(resized_image)
 
 # convert each pixel to grayscale
-def grafify(image):
+def grayify(image):
     grayscale_image = image.convert("L")
     return(grayscale_image)
 
@@ -22,11 +22,25 @@ def pixels_to_ascii(image):
     characters = "".join([ASCII_CHARS[pixels//25] for pixel in pixels])
     return (characters)
 
-def main():
+def main(new_width=100):
     path=input("Enter a valid pathname to an image:\n")
     try:
         image=PIL.Image.open(path)
     except:
         print(path,"is not a valid pathname to an image")
+
+    # convert image to ASCII
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+
+    # format
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+
+    # print result
+    print(ascii_image)
+
+    # save result to "ascii_image.txt"
+    with open("ascii_image.txt","w")as f:
+        f.write(ascii_image)
 
 main()
